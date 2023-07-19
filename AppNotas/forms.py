6312af3e285fa4e1acd09816b602fe7857bdf334
WillieRizzo <71811987+WillieRizzo@users.noreply.github.com>
2023-07-18 21:45:19 -0400

    prueba formlurario clase

--- AppNotas/forms.py
+++ AppNotas/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Nota
+from .models import Nota, Producto
 
 class NotaForm(forms.ModelForm):
     class Meta:
         model = Nota
         fields = ["nota"]
+
+
+class CrearNuevoLibro(forms.Form):
+    title = forms.CharField(label="Titulo del libro", max_length=200)
+    description = forms.CharField(label="Descriccion del libro", widget=forms.Textarea)
